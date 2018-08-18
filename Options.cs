1b09955c31c778eb6315ee3b5a910cdf279867af
Rogier Reedijk <github@progz.nl>
2018-08-18 21:42:10 +0200

    Add commandline options class and parse commandline.

---
+++ Options.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace wordpress_to_hugo_and_staticman_converter
+{
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Locatio of XML export from Wordpress to convert.")]
+        public string Input { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 
 namespace wordpress_to_hugo_and_staticman_converter
 {
@@ -6,7 +7,12 @@ namespace wordpress_to_hugo_and_staticman_converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    Console.WriteLine($"Start processing '{o.Input}'...");
+                    Console.WriteLine("Done.");
+                });
         }
     }
 }
