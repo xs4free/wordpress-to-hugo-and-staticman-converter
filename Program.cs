ecaebdb0dc57845ba675aebb9fd4d481d5c53b05
Rogier Reedijk <github@progz.nl>
2018-08-18 21:15:30 +0200

    Add .gitignore and empty .NET Core console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wordpress_to_hugo_and_staticman_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
